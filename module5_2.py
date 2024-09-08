d47c5881437678ee71c67ab41870ec694f3cbd2e
ekaterina-es <k.kathy95@mail.ru>
2024-09-08 18:28:36 +0300

    Update module5_2.py

--- module5_2.py
+++ module5_2.py
@@ -2,7 +2,14 @@ class House:
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-
+    
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor <= 1:
+            print("Такого этажа не существует")
+        else:
+            for i in range(new_floor):
+                print(i+1)
+    
     def __len__(self):
         return self.number_of_floors
 
